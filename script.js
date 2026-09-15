9342c16f16a401b8214f9433a37e5d6e05be6cc2
Ihsanullah Mahboobi <iumahboobi@gmail.com>
2026-09-15 10:33:25 +0200

    boiler_plate

---
+++ script.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// TODO: State variables
+
+// TODO: DOM references
+
+// TODO: Math functions (add, subtract, multiply, divide)
+
+// TODO: operate() function
+
+// TODO: Display helpers (updateDisplay, formatNumber)
+
+// TODO: Input handlers (number, operator, equals, clear, decimal, backspace)
+
+// TODO: Keyboard support
+
+// TODO: Event listeners + boot
