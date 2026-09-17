1c32b4a6ac2136218888e07ae9a77640d477ddf8
Pedro Millán <millanpedrop@gmail.com>
2026-09-17 11:16:49 +0200

    Implement calculator input and operation handlers

--- script.js
+++ script.js
@@ -1,17 +1,112 @@
 'use strict';
 
-// TODO: State variables
+//State variables
+let currentInput = "0";
+let previousInput = null;
+let operator = null;
+let isReset = false;
 
-// TODO: DOM references
+//DOM references
+const currentDisplay = document.querySelector(".display__current");
+const previousDisplay = document.querySelector(".display__previous");
 
-// TODO: Math functions (add, subtract, multiply, divide)
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+
+const decimalButton = document.querySelector('[data-action="decimal"]');
+const equalsButton = document.querySelector('[data-action="equals"]');
+
+//Math functions (add, subtract, multiply, divide)
 
 // TODO: operate() function
 
 // TODO: Display helpers (updateDisplay, formatNumber)
+function updateDisplay() {
+    currentDisplay.textContent = currentInput;
+
+    if (previousInput !== null && operator !== null) {
+        previousDisplay.textContent =
+            `${previousInput} ${operator}`;
+    } else {
+        previousDisplay.textContent = "";
+    }
+}
 
 // TODO: Input handlers (number, operator, equals, clear, decimal, backspace)
+function handleNumber(pushedNumber) {
+
+    if (isReset === true) {
+        currentInput = pushedNumber;
+        isReset = false;
+    } else {
+        if (currentInput === "0") {
+            currentInput = pushedNumber;
+        } else {
+        currentInput = currentInput + pushedNumber;
+        }
+    }
+    updateDisplay();
+}
+
+function handleDecimal() {
+
+    if (isReset === true) {
+        currentInput = "0.";
+        isReset = false;
+    } else if (currentInput.includes(".")){
+        return;
+    } else {
+        currentInput = currentInput + ".";
+    }
+    updateDisplay();
+}
+
+function handleEquals() {
+
+    if (previousInput === null || operator === null || isReset === true){
+        return;
+    }
+    const inputValue = Number(currentInput);
+
+    const result = operate(operator, previousInput, inputValue);
+
+    currentInput = String(result);
+    previousInput = null;
+    operator = null;
+    isReset = true;
+
+    updateDisplay();
+}
+
+function handleOperator(selectedOperator) {
+    const inputValue = Number(currentInput);
+
+    if (previousInput === null) {
+        previousInput = inputValue;
+    }else if (operator !== null && isReset === false){
+        const result = operate(operator, previousInput, inputValue);
+
+        currentInput = String(result);
+        previousInput = result;
+    }
+
+    operator = selectedOperator;
+    isReset = true;
+
+    updateDisplay();
+}
+
 
 // TODO: Keyboard support
 
 // TODO: Event listeners + boot
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        handleNumber(button.dataset.number);
+    });
+});
+
+decimalButton.addEventListener("click", handleDecimal);
+equalsButton.addEventListener("click", handleEquals);
+
+
